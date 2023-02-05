c686fd182df9a1386db82e9e40c90be608b6306a
Sean Rawson <seancc8@gmail.com>
2023-02-05 08:39:11 -0500

    Refactor SearchHistory class

--- src/main/java/cs622/hw2/cli/command/SearchHistory.java
+++ src/main/java/cs622/hw2/cli/command/SearchHistory.java
@@ -1,42 +1,68 @@
+/**
+ * Name: Sean Rawson
+ * Date: 2/4/2023
+ * Class: CS-622 Spring 2023
+ * Assignment 3
+ * Description: This file contains a search history class which store all searches
+ */
 package cs622.hw2.cli.command;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.function.Supplier;
-import java.time.LocalDateTime;
 
+
+/**
+ * Search history class. Stores keywords and all searches performed
+ * for that keyword in a HashMap.
+ */
 public class SearchHistory {
 	
-	private Map<String, Search> searches;
+	private Map<String, List<Search>> searches;
 	
+	/**
+	 * Default constructor. Initialize HashMap.
+	 */
 	public SearchHistory() {
 		searches = new HashMap<>();
 	}
 	
-	public Map<String, Search> getSearches() {
+	
+	/**
+	 * Get all of the searches in this history.
+	 * @return Map of keywords to lists of search objects
+	 */
+	public Map<String, List<Search>> getSearches() {
 		return searches;
 	}
 	
 	
 	/**
 	 * Add a search to the search history
-	 * @param key search string for this search
+	 * @param searchString search string for this search
 	 * @param search search object containing search state/parameters
-	 * @return
+	 * @return true if the search was successfully added to the history, false otherwise
 	 */
 	public boolean put(String searchString, Search search) {
-		String timeStamp = LocalDateTime.now().toString();
 		boolean success = false;
-		try {
-			searches.put(searchString + "_" + timeStamp, search);
+		if (searches.containsKey(searchString)) {
+			success = searches.get(searchString).add(search);
+		}
+		else {
+			List<Search> searchList = new ArrayList<>();
+			searchList.add(search);
+			searches.put(searchString, searchList);
 			success = true;
-		} catch (Exception e) {
-			System.out.println("error adding search to search history");
-			success = false;
 		}
 		return success;
 	}
-
 	
-	public void execute() {}
+	
+	public void showAllSearches() {
+		searches.entrySet().stream().forEach(entry -> {
+			System.out.println(String.format("Searches for keyword \"%s\"%n--------------------%n", entry.getKey()));
+			entry.getValue().stream().forEach(System.out::println);
+		});
+	}
 }
