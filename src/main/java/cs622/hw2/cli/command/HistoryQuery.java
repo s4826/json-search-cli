27993d24e32e95955f7bad8f6563a28f774f6700
Sean Rawson <seancc8@gmail.com>
2023-02-06 11:55:51 -0500

    Add file/method javadocs and comments

--- src/main/java/cs622/hw2/cli/command/HistoryQuery.java
+++ src/main/java/cs622/hw2/cli/command/HistoryQuery.java
@@ -1,18 +1,76 @@
+/**
+ * Name: Sean Rawson
+ * Date: 2/6/2023
+ * Class: CS-622 Spring 2023
+ * Assignment 3
+ * Description: This file contains functionality for history queries.
+ */
 package cs622.hw2.cli.command;
 
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.concurrent.Callable;
+
+
+/**
+ * Class to encapsulate a history query.
+ */
 public class HistoryQuery implements Command {
 	
 	private SearchHistory historyTarget;
+	private Map<String, Callable<Void>> historyFunctions;
 	
-	public HistoryQuery() {}
+	public HistoryQuery() {
+		historyFunctions = new HashMap<>();
+	}
 	
+	
+	/**
+	 * Set the SearchHistory object that will be the target for this history query
+	 */
 	public void setHistoryTarget(SearchHistory historyTarget) {
 		this.historyTarget = historyTarget;
 	}
 	
+	
+	/**
+	 * Run the query represented by this history query object.
+	 */
 	@Override
 	public void run() {
-		historyTarget.showAllSearches();
+		Scanner in = new Scanner(System.in);
+		String historyCommand = getOptFromInput(in, System.out);
+		try {
+			historyFunctions.get(historyCommand).call();
+		} catch (Exception e) {
+			System.out.println("Error running history command");
+		}
 	}
+	
+	
+	/**
+	 * Get the history query option from the specified input source, usually STDIN.
+	 * @param in Scanner to read input stream which will supply the option
+	 * @param out output stream to display prompts
+	 * @return history query option as a string
+	 */
+	protected String getOptFromInput(Scanner in, PrintStream out) {
+		List<String> availableOpts = Arrays.asList(new String[]{"a", "all", "u", "unique", "t", "time"});
+		out.print("Select a stats option. Available stats are:\n");
+		String optionString = String.format("%s%n%s%n%s%n? ",
+					"Print all searches (a/all)",
+					"Number of unique search terms with associated frequency (u/unique)",
+					"Timestamps of all searches (t/time)");
+		out.print(optionString);
 
+		String input = in.nextLine();
+		while (!availableOpts.contains(input))
+			out.printf("Invalid option. Please choose one of the following:%n%s", optionString);
+
+		return input;
+	}
 }
--- src/main/java/cs622/hw2/cli/command/HistoryQueryFactory.java
+++ src/main/java/cs622/hw2/cli/command/HistoryQueryFactory.java
@@ -1,5 +1,16 @@
+/**
+ * Name: Sean Rawson
+ * Date: 2/6/2023
+ * Class: CS-622 Spring 2023
+ * Assignment 3
+ * Description: This file contains functionality for generating search history queries.
+ */
 package cs622.hw2.cli.command;
 
+
+/**
+ * Class to generate history queries against a SearchHistory object
+ */
 public class HistoryQueryFactory implements CommandFactory {
 	private SearchHistory historyTarget;
 	
@@ -8,6 +19,10 @@ public class HistoryQueryFactory implements CommandFactory {
 	}
 	
 	
+	/**
+	 * Create and return a history query.
+	 * @return history query as Command object
+	 */
 	public Command create() {
 		HistoryQuery historyQuery = new HistoryQuery();
 		historyQuery.setHistoryTarget(historyTarget);
