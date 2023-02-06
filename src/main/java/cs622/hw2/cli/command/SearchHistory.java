3d1b026778cef7fb439e9fc1c4ca79939294a7e7
Sean Rawson <seancc8@gmail.com>
2023-02-06 13:06:23 -0500

    Print search history instead of returning to caller

--- src/main/java/cs622/hw2/cli/command/SearchHistory.java
+++ src/main/java/cs622/hw2/cli/command/SearchHistory.java
@@ -30,15 +30,6 @@ public class SearchHistory {
 	
 	
 	/**
-	 * Get all of the searches in this history.
-	 * @return Map of keywords to lists of search objects
-	 */
-	public Map<String, List<Search>> getSearches() {
-		return searches;
-	}
-	
-	
-	/**
 	 * Add a search to the search history
 	 * @param searchString search string for this search
 	 * @param search search object containing search state/parameters
@@ -59,10 +50,45 @@ public class SearchHistory {
 	}
 	
 	
-	public void showAllSearches() {
+	/**
+	 * Get all of the searches in this history.
+	 * @return Map of keywords to lists of search objects
+	 */
+	protected Map<String, List<Search>> getAllSearches() {
+		return searches;
+	}
+	
+	
+	/**
+	 * Print all unique search terms and their frequencies.
+	 */
+	public void printUniqueSearchTerms() {
+		System.out.println("Search terms and frequencies");
+		searches.entrySet().forEach(entry -> {
+			System.out.printf("%s: %d times%n", entry.getValue().size());
+		});
+	}
+	
+	
+	/**
+	 * Print the time stamps of all search terms in this search history.
+	 */
+	public void printSearchTimeStamps() {
+		searches.entrySet().stream().forEach(entry -> {
+			System.out.printf("Searches for \'%s\'%n----------------%n", entry.getKey());
+			entry.getValue().forEach(System.out::println);
+		});
+	}
+	
+	
+	/**
+	 * Print all searches in this search history.
+	 */
+	public void printAllSearches() {
 		searches.entrySet().stream().forEach(entry -> {
 			System.out.println(String.format("Searches for keyword \"%s\"%n--------------------", entry.getKey()));
 			entry.getValue().stream().forEach(System.out::println);
 		});
 	}
+	
 }
